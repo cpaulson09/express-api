ab5ecd3e82b193cd5c1d8e43b11ad574d95fc29c
cpaulson09 <cpaulson09@hotmail.com>
2021-03-16 14:20:18 -0600

    added users for random users route

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors")
 const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 3000
-const axios = require(axios)
+const axios = require('axios')
 
 //middle ware
 app.use(bodyParser.json())
@@ -19,16 +19,16 @@ app.get('/', (req, res) => {
 })
 
 app.get('/users', (req, res) => {
-    res.send('here')
-    // axios.get(`https://randomuser.me/api/?results=50`)
-    // .then(response => {
-    //     if (response.data){
-    //         res.status(200).send(response.data.results)
-    //     }
-    // })
-    // .catch(err => {
-    //     console.log(err)
-    // })
+    // res.send('here')
+    axios.get(`https://randomuser.me/api/?results=50`)
+    .then(response => {
+        if (response.data){
+            res.status(200).send(response.data.results)
+        }
+    })
+    .catch(err => {
+        console.log(err)
+    })
 })
 
 app.listen(port, () => {console.log(`${port}`)})
